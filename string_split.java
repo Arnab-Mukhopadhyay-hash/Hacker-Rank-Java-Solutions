febe0966cdfb7301c22c6d02924567280261fff7
Arnab-Mukhopadhyay-hash <84343247+Arnab-Mukhopadhyay-hash@users.noreply.github.com>
2023-03-27 18:30:32 +0000

    Java String Tokens

--- string_split.java
+++ string_split.java
@@ -4,14 +4,16 @@ import java.util.regex.*;
 public class string_split {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine().trim();
+        String str = scan.nextLine().trim(); // we take the whole string as input and trim the spaces at the end and
+                                             // starting.
         scan.close();
 
         if (str.length() == 0) {
             System.out.println(0);
             return;
         }
-        String[] str_arr = str.split("[\\s+!,.'@?_]+");
+        String[] str_arr = str.split("[\\s+!,.'@?_]+"); // this will split the string at ! , . ? ' _ @ and at blank
+                                                        // spaces, and make an array of strings.
         System.out.println(str_arr.length);
         for (String i : str_arr) {
             System.out.println(i);
